09c7288986d784207dc628e6538c56ba681e377b
Fernando Daniel Hernandez Amaya <fhernandezamaya1@gmail.com>
2023-02-06 19:35:43 -0600

    configuracion inicial

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const path = require('path');
 
+//Initializations
 const app = express();
 
+
+//Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'))
+
+//Middlewares
+app.use(express.urlencoded({extended: false}));
 
 //Routes
 app.get('/', (req,res) => {
     res.send('hello world');
 });
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
